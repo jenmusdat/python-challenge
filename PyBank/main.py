45d0abd0bce95203f13afc50a6954162ca97bc43
Robert and Jenny <jenny_nickles@hotmail.com>
2020-10-09 19:55:05 -0700

    pybank first

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,65 @@
-import budget
+import os
 import csv
 
-output_path = os.path.join ("..", "output", "new.csv")
+csvpath = os.path.join ("Resources", "budget_data.csv")
 
-with open (output_path, "w") as csvfile:
-    
-    csvwriter = csv.writer(csvfile, delimiter=',')
 
+outpath = os.path.join ("Analysis", "budget_data.txt")
+
+
+with open (csvpath) as filepath:
+    csvreader = csv.reader(filepath, delimiter=',')
+
+    print(csvreader)
+    #
+    csv_header = next(csvreader)
+
+    print(f"CSV Header: {csv_header}")
+
+    total=0
+    totalprofitlosses=0
+    totalchange=0
+    previous=0
+    greatestincrease=0
+    greatestmonth = ""
+    greatestdecrease=99999999999
+    decreasemonth = ""
+
+    for row in csvreader:
+        total=total+1
+        totalprofitlosses=totalprofitlosses + int(row[1])
+
+        if total > 1:
+
+            change=int(row[1])-previous
+            totalchange=totalchange + change
+            if change > greatestincrease:
+                greatestincrease=change
+                greatestmonth = row[0]
+            
+            if change < greatestdecrease:
+                greatestdecrease = change
+                decreasemonth = row[0]
+
+        previous=int(row[1])
+        
+        
+       # print(row[1])
+
+print("Financial Analysis")
+print("--------------------")
+print("Total Months:  "+str(total))
+print("Total:  $"+str(totalprofitlosses))
+print("Average Change: $" +str(totalchange/(total-1)))
+print("Greatest Increase:  " + greatestmonth +" $"+str(greatestincrease))
+print("Greatest Decrease:  " + decreasemonth +" $"+str(greatestdecrease))
+
+with open (outpath, "w") as textfile:
+    textfile.write ("Financial Analysis\n")
+    textfile.write ("--------------------\n")
+    textfile.write("Total Months:  "+str(total)+"\n")
+    textfile.write("Total:  $"+str(totalprofitlosses)+"\n")
+    textfile.write("Average Change: $" +str(totalchange/(total-1))+"\n")
+    textfile.write("Greatest Increase:  " + greatestmonth +" $"+str(greatestincrease)+"\n")
+    textfile.write("Greatest Decrease:  " + decreasemonth +" $"+str(greatestdecrease)+"\n")
     
