496fe1b3131d5cf65d345bc73020c53b17310089
Robert and Jenny <jenny_nickles@hotmail.com>
2020-10-11 11:01:33 -0700

    adding print lines

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,8 +10,28 @@ outpath = os.path.join ("Analysis", "election_data.txt")
 with open (csvpath) as filepath:
     csvreader = csv.reader(filepath, delimiter=',')
 
-    print(csvreader)
-    #
+    #print(csvreader)
+    
     csv_header = next(csvreader)
-
+    
     print(f"CSV Header: {csv_header}")
+
+    #set my variables
+    total_votes = 0
+    candidates = dict()
+
+
+
+
+
+
+
+
+print("Election Results")
+print("------------------------------")
+print("Total Votes: ")
+print("------------------------------")
+print(candidates)
+print("------------------------------")
+print("Winner:  ")
+print("------------------------------")
