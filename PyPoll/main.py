0644cb42fd4f8191aaf4fb9c190a06053c2603ad
Robert and Jenny <jenny_nickles@hotmail.com>
2020-10-12 16:34:29 -0700

    print lines added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,6 +13,7 @@ outpath = os.path.join ("Analysis", "election_data.txt")
 
 # create empty dictionary to store candidates
 candidatelist = {}
+
 with open (csvpath) as filepath:
     csvreader = csv.reader(filepath, delimiter=',')
 
@@ -26,9 +27,10 @@ with open (csvpath) as filepath:
     vote_count=0
     total_votes=0
     total=0
-    greatestvotecount=0
-    greatvotewinner=""
     candidate=""
+    candidate_name=""
+    vote=0
+    candidatechange=0
 
     for row in csvreader:
         vote_count = vote_count + 1
@@ -38,68 +40,20 @@ with open (csvpath) as filepath:
         else: 
             candidatelist[candidate] += 1
 
-    
-    for row in csvreader:
-            
-            total_votes = total_votes+1
-
-            candidate_name = row[2]
-
-            if not candidate_name in candidate:
-                candidate.update({row[2] : candidate_name})
-     # for row in reader:
-    #for A what will we do? sum the total number of rows
-        #total_votes +=1
-   
-    #for unique_candidates:
-    #all_candidate_with_duplicate.append(row[2])
-    #Candidate = "Khan"
-    #dictionary = {"Candidate":"Khan"
-#}
-   # if Candidate in dictionary.values(): 
-
-    #totaluniqueKhan.count
-    #count = 0
-    #all_candidate_with_duplicate + []
-    #create a dictionary
-    #candidate_totals = {}
-
-#with open file to load as poll_csv
-    #reader = csv (poll_csv)
-
-    #discard header if you want
-    #next on the reader object
-
-    #the goal is to loop everything once
- 
-   # for uniquecandidates 
-#    all_candidate_with_duplicate.append(row[2])
-#
-#    for total number of votes per candidate
-#    add a dictionary with a key above the with
-#    if candidate_totals.get(row[2]):
-#        candidate_totals[row[2]]+=1
-#    else:
-#   candidate_totals[row[2]] = 0
-
-
-#for candidate_name,vote_toatl in candidate_totals:
- #   print(candidate_name).items
-#print(v/total_votes*100)
-#print(max(candidate_name))
-
-    #{
-    #    "candidate_a": 2,
-    #    "candidate_b": 1,
-#previous=int(row[1])
 
 #display my results
 print("Election Results")
 print("------------------------------")
-print("Total Votes: " +str(total_votes))
+print("Total Votes: ", str(vote_count))
 print("------------------------------")
-print(candidatelist)
-print(vote_count)
+winner_votes=0
+winner=""
+for candidate in candidatelist:
+    print((f"{candidate}: {candidatelist[candidate]vote_count)*100:.3f}%({candidatelist[candidate]})")
+    if candidatelist[candidate] > winner_votes:
+        winnercount=candidatelist[candidate]
+        winner=candidate
 print("------------------------------")
-print("Winner:  ")
+print(f"Winner: {greatvotewinner} ")
 print("------------------------------")
+
