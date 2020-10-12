ba9804e3d7a421394953c17b1415d9a61b1c1db5
Robert and Jenny <jenny_nickles@hotmail.com>
2020-10-11 18:38:49 -0700

    comments added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,69 @@
+#import my needed modules
 import os
 import csv
 
+# connect the csv path to Python so I can write my code
 csvpath = os.path.join ("Resources", "election_data.csv")
 
-
+#create the path to send a txt file to the repository
 outpath = os.path.join ("Analysis", "election_data.txt")
-
+#A. total votes cast
+#B. unique candidates list
+#C. total number of votes each candidate got
+#D. percentage of candidates
+#E. the winner of the election based on popular vote
+#voterid, county, candidate
+#1, county_a, candidate-a
+#2, county_a, candidate_a
+#3, county_b, candidate_b
 
 with open (csvpath) as filepath:
     csvreader = csv.reader(filepath, delimiter=',')
 
     #print(csvreader)
     
-    csv_header = next(csvreader)
+    #csv_header = next(csvreader)
     
-    print(f"CSV Header: {csv_header}")
+   # print(f"CSV Header: {csv_header}")
+    #create the dictionary
+    candidates_list = {Candidate}
+    Candidate = candidates_list
+      #set my variables
+     # for row in reader:
+    #for A what will we do? sum the total number of rows
+    # total_votes = 0
+    total_votes +=1
+   
+     #  For unique candidates 
+    #all_candidate_with_duplicate.append(row[2])
+    candidates_list = (Candidate)
+    #x = dict_candidates["Candidate"]
 
-    #set my variables
-    total_votes = 0
-    candidates = dict()
+    #if "Khan" is "Khan"  
+    #count = 1
+   # if "Khan" is not "Khan"
+    #count = 0
+    all_candidate_with_duplicate + []
+    #create a dictionary
+    candidate_totals = {}
+
+#with open file to load as poll_csv
+    #reader = csv (poll_csv)
+
+    #discard header if you want
+    #next on the reader object
+
+    #the goal is to loop everything once
+ 
+#    For unique candidates 
+#    all_candidate_with_duplicate.append(row[2])
+#
+#    for total number of votes per candidate
+#    add a dictionary with a key above the with
+#    if candidate_totals.get(row[2]):
+#        candidate_totals[row[2]]+=1
+#    else:
+#   candidate_totals[row[2]] = 0
 
 
 
@@ -27,11 +72,24 @@ with open (csvpath) as filepath:
 
 
 
+
+
+
+for candidate_name,vote_toatl in candidate_totals:
+    print(candidate_name).items
+print(v/total_votes*100)
+print(max(candidate_name))
+
+    #{
+    #    "candidate_a": 2,
+    #    "candidate_b": 1,
+   
+#display my results
 print("Election Results")
 print("------------------------------")
-print("Total Votes: ")
+print("Total Votes: " +str(total_votes))
 print("------------------------------")
-print(candidates)
+print(set (all_candidates_with_duplicate))
 print("------------------------------")
 print("Winner:  ")
 print("------------------------------")
